59d6efbaa105df8640703d7cfec7d8961cf0af39
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-10-10 22:00:00 -0300

    Add Advance X Steps button

--- src/components/GameOfLife.jsx
+++ src/components/GameOfLife.jsx
@@ -9,19 +9,28 @@ const GameOfLife = ({ rows, columns }) => {
 
   const [currentGrid, setCurrentGrid] = useState(createEmptyGrid());
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [stepsToAdvance, setStepsToAdvance] = useState(2);
+  const [stopAt, setStopAt] = useState(-1);
 
   const nextGeneration = useCallback(() => {
     const nextGrid = evolveGrid(currentGrid);
 
+    setCurrentStep(currentStep + 1);
     setCurrentGrid(nextGrid);
-  }, [currentGrid]);
+  }, [currentGrid, currentStep]);
 
   const togglePlay = () => {
+    setStopAt(-1);
     setIsPlaying(!isPlaying);
   };
 
   useEffect(() => {
     const intervalId = setInterval(nextGeneration, 500);
+
+    if (stopAt > -1 && currentStep >= stopAt) {
+      setIsPlaying(false);
+    }
 
     if (!isPlaying) {
       clearInterval(intervalId);
@@ -30,7 +39,7 @@ const GameOfLife = ({ rows, columns }) => {
     return () => {
       clearInterval(intervalId);
     };
-  }, [isPlaying, nextGeneration]);
+  }, [currentStep, isPlaying, nextGeneration, stopAt]);
 
   const toggleCell = (rowIndex, colIndex) => {
     const gridCopy = [...currentGrid];
@@ -39,25 +48,47 @@ const GameOfLife = ({ rows, columns }) => {
     setCurrentGrid(gridCopy);
   };
 
+  const handleAdvanceSteps = () => {
+    setStopAt(currentStep + stepsToAdvance);
+    setIsPlaying(true);
+  };
+
   return (
     <>
-      <div style={{ marginBottom: "30px" }}>
+      <div style={{ margin: "20px" }}>
+        {createEmptyGrid().map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: "flex" }}>
+            {row.map((_, colIndex) => (
+              <Cell
+                key={`${rowIndex}-${colIndex}`}
+                rowIndex={rowIndex}
+                colIndex={colIndex}
+                cellValue={currentGrid[rowIndex][colIndex]}
+                toggleCell={toggleCell}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <div>
         <button onClick={nextGeneration}>Next State</button>
         <button onClick={togglePlay}>{isPlaying ? "Stop" : "Play"}</button>
+        <button onClick={handleAdvanceSteps}>
+          Advance {stepsToAdvance} steps
+        </button>
       </div>
-      {createEmptyGrid().map((row, rowIndex) => (
-        <div key={rowIndex} style={{ display: "flex" }}>
-          {row.map((_, colIndex) => (
-            <Cell
-              key={`${rowIndex}-${colIndex}`}
-              rowIndex={rowIndex}
-              colIndex={colIndex}
-              cellValue={currentGrid[rowIndex][colIndex]}
-              toggleCell={toggleCell}
-            />
-          ))}
-        </div>
-      ))}
+      <label>
+        Auto advance steps:
+        <input
+          type="number"
+          min="2"
+          max="1000"
+          step="1"
+          value={stepsToAdvance}
+          onChange={(e) => setStepsToAdvance(Number(e.target.value))}
+        />
+      </label>
+      <p>Current step: {currentStep}</p>
     </>
   );
 };
